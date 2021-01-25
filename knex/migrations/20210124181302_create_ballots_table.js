e7392e0da1ae55c0da79edac14b917e86f78c16a
sacOO7 <sachinshinde7676@gmail.com>
2021-01-25 20:20:42 +0530

    Updated ballot migration script

--- knex/migrations/20210124181302_create_ballots_table.js
+++ knex/migrations/20210124181302_create_ballots_table.js
@@ -4,10 +4,10 @@ exports.up = function(knex) {
     return Promise.resolve(
         knex.schema.createTable(tableName, (tbl) => {
             tbl.string(table.columns.id, 44).notNullable();
-            tbl.string(table.columns.type, 20).nullable();
+            tbl.string(table.columns.type, 20).notNullable();
             tbl.string(table.columns.voterAddress, 80).notNullable();
             tbl.string(table.columns.delegateAddress, 80).notNullable()
-            tbl.boolean(table.columns.active).defaultTo(false).notNullable();
+            tbl.boolean(table.columns.active).defaultTo(true).notNullable();
             tbl.primary([table.columns.id]);
             tbl.unique([table.columns.voterAddress, table.columns.delegateAddress]);
         }),

17257e4fec9576e90b6d38ddb025e9fd24eb2a3b
sacOO7 <sachinshinde7676@gmail.com>
2021-01-25 19:48:05 +0530

    Updated ballots migration script, set type to be nullable, set active flag as default to false

--- knex/migrations/20210124181302_create_ballots_table.js
+++ knex/migrations/20210124181302_create_ballots_table.js
@@ -4,11 +4,12 @@ exports.up = function(knex) {
     return Promise.resolve(
         knex.schema.createTable(tableName, (tbl) => {
             tbl.string(table.columns.id, 44).notNullable();
-            tbl.string(table.columns.type, 20).notNullable();
+            tbl.string(table.columns.type, 20).nullable();
             tbl.string(table.columns.voterAddress, 80).notNullable();
             tbl.string(table.columns.delegateAddress, 80).notNullable()
-            tbl.boolean(table.columns.active).notNullable();
-            tbl.primary([table.columns.voterAddress, table.columns.delegateAddress]);
+            tbl.boolean(table.columns.active).defaultTo(false).notNullable();
+            tbl.primary([table.columns.id]);
+            tbl.unique([table.columns.voterAddress, table.columns.delegateAddress]);
         }),
     )
 };

0bd5e3b37c3f77560ef16df3fac4b6bb955afb0c
sacOO7 <sachinshinde7676@gmail.com>
2021-02-09 02:08:26 +0530

    Removed unencessary db bingint aka string to int conversion

--- knex/knex.js
+++ knex/knex.js
@@ -2,12 +2,11 @@ const {isLocal} = require('../src/utils');
 const environment = process.env.ENVIRONMENT || 'development';
 const config = require('../knexfile.js')[environment];
 
-var types = require('pg').types;
-
-// todo - need to check if parseInt satisfy bigint requirement
-types.setTypeParser(20, function (val) {
-  return parseInt(val, 10);
-});
+// Needed if BigInt values need to be converted to integer from string
+// var types = require('pg').types;
+// types.setTypeParser(20, function (val) {
+//   return parseInt(val, 10);
+// });
 
 const knex = require('knex')(config);
 if (isLocal(environment)) {

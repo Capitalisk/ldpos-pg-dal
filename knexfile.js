5380fa9420bb1012e27366498b5bf230f54a8071
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2021-02-19 14:52:26 +0100

    Cleanup code style

--- knexfile.js
+++ knexfile.js
@@ -3,23 +3,23 @@
 module.exports = {
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'ldpos',
-    password : 'ldpos',
-    database : 'ldpos_test',
+    host: '127.0.0.1',
+    user: 'ldpos',
+    password: 'ldpos',
+    database: 'ldpos_test',
     port: '5432',
     charset: 'utf8',
     timezone: 'UTC',
   },
   pool: {
     min: 2,
-    max: 10
+    max: 10,
   },
   migrations: {
     tableName: 'knex_migrations',
     directory: __dirname + '/knex/migrations',
   },
   seeds: {
-    directory: __dirname + '/knex/seeds'
-  }
+    directory: __dirname + '/knex/seeds',
+  },
 };

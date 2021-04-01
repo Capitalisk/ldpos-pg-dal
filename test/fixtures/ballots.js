4b6bd99e84bf9801ec0e8bf8b714215662ca978d
sacOO7 <sachinshinde7676@gmail.com>
2021-04-02 02:12:07 +0530

    Added data fixtures for writing tests

--- test/fixtures/ballots.js
+++ test/fixtures/ballots.js
@@ -1,2 +1,52 @@
-const ballots = [];
+const ballots = [
+    {
+        "id": "5a17114dca1b780981c590a9c69e54b6c06e9a71",
+        "type": "vote",
+        "voterAddress": "clskbde48efe4fb34f91f3bfad4e4e8feb081b302b0f",
+        "delegateAddress": "clskbde48efe4fb34f91f3bfad4e4e8feb081b302b0f",
+        "active": true
+    },
+    {
+        "id": "ac5df1d5cc44e4d80748babc48d4e843387de3bd",
+        "type": "vote",
+        "voterAddress": "clskd98aa045ac36d9193cbcca060b8a8b0f1862aae2",
+        "delegateAddress": "clskbde48efe4fb34f91f3bfad4e4e8feb081b302b0f",
+        "active": true
+    },
+    {
+        "id": "4a1a60608ba2e3d8af96a65f29163e511214f8d8",
+        "type": "vote",
+        "voterAddress": "clskbc84c66b69477c2160ef2eb5c92579840fa0ef3f",
+        "delegateAddress": "clskbde48efe4fb34f91f3bfad4e4e8feb081b302b0f",
+        "active": true
+    },
+    {
+        "id": "6acf21b2db3a0a44992e8c763ed3ab587e79f7d7",
+        "type": "vote",
+        "voterAddress": "clskb231cb14eff2e98c72ebae22fcf06c1e1a6f08fd",
+        "delegateAddress": "clskbde48efe4fb34f91f3bfad4e4e8feb081b302b0f",
+        "active": true
+    },
+    {
+        "id": "d4f42bcc4a6071f452100eb34ef03e23dc0a00ba",
+        "type": "vote",
+        "voterAddress": "clskbde48efe4fb34f91f3bfad4e4e8feb081b302b0f",
+        "delegateAddress": "clskbc84c66b69477c2160ef2eb5c92579840fa0ef3f",
+        "active": true
+    },
+    {
+        "id": "6f4bf69eeb3b20ff5c9f7ee3a09c1fb30e7bbf21",
+        "type": "vote",
+        "voterAddress": "clsk65d1fcabf86fc5eef5844b6065d786be4ec3fcd9",
+        "delegateAddress": "clskbde48efe4fb34f91f3bfad4e4e8feb081b302b0f",
+        "active": true
+    },
+    {
+        "id": "8bd1ecb6a5c83a218fa519e493a7a1b4ad0fc7da",
+        "type": "vote",
+        "voterAddress": "clsk65d1fcabf86fc5eef5844b6065d786be4ec3fcd9",
+        "delegateAddress": "clskbc84c66b69477c2160ef2eb5c92579840fa0ef3f",
+        "active": true
+    }
+];
 module.exports = ballots;

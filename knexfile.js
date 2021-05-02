2caca30f2cbbdf4b3f151c9af0582375f6be4900
sacOO7 <sachinshinde7676@gmail.com>
2021-05-03 02:45:46 +0530

    Updated knexfile to use sqlite module, when USE_SQLITE is set

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,29 @@
 // Update with your config settings.
 
 module.exports = {
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'ldpos',
-    password: 'ldpos',
-    database: 'ldpos_test',
-    port: '5432',
-    charset: 'utf8',
-    timezone: 'UTC',
-  },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-    directory: __dirname + '/knex/migrations',
-  },
-  seeds: {
-    directory: __dirname + '/knex/seeds',
-  },
+    client: process.env.USE_SQLITE ? 'sqlite3' : 'pg',
+    connection: process.env.USE_SQLITE ?
+        {
+            filename: 'db.sqlite3'
+        } :
+        {
+            host: '127.0.0.1',
+            user: 'ldpos',
+            password: 'ldpos',
+            database: 'ldpos_test',
+            port: '5432',
+            charset: 'utf8',
+            timezone: 'UTC',
+        },
+    pool: {
+        min: 2,
+        max: 10,
+    },
+    migrations: {
+        tableName: 'knex_migrations',
+        directory: __dirname + '/knex/migrations',
+    },
+    seeds: {
+        directory: __dirname + '/knex/seeds',
+    },
 };
